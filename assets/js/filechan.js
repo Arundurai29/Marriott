17c13b2d1c76ceb6c08b464f56fe7c88541a69bd
Arundurai29 <114252340+Arundurai29@users.noreply.github.com>
2023-08-04 02:26:27 +0530

    Add files via upload

--- assets/js/filechan.js
+++ assets/js/filechan.js
@@ -5,3 +5,19 @@ $(function(){
   $(function(){
     $(".navbar").load("header.html"); 
   });
+
+  $(function(){
+    $(".sidebar1").load("sidebar2.html"); 
+  });
+  $(function(){
+    $(".sidebar3").load("sidebar4.html"); 
+  });
+  $(function(){
+    $(".sidebar4").load("sidebar5.html"); 
+  });
+  $(function(){
+    $(".sidebar2").load("sidebar3.html"); 
+  });
+  $(function(){
+    $(".themer").load("theme.html"); 
+  });
